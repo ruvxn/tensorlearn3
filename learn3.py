30847060853ffcb791d22f0acc44539581b4fb95
Ruveen Jayasinghe <ruveenjay@gmail.com>
2025-02-05 11:35:10 +1100

    tensor learn 3

--- learn3.py
+++ learn3.py
@@ -1,1 +1,15 @@
 #learn3
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+# Load the data
+data = pd.read_csv('dataset/bike_sharing_daily.csv')
+
+print(data.head(10))
+
+
+
